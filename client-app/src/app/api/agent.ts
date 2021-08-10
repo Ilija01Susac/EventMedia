5f82b4e1e9974e56551e0213a9a9e6f7b760734b
Ilija01Susac <Isusac7695@gmail.com>
2021-08-10 19:56:24 +0200

    finished CRUD

---
+++ client-app/src/app/api/agent.ts
@@ -0,0 +1,43 @@
+import axios, { AxiosResponse } from "axios";
+import { Activity } from "../models/activity";
+
+const sleep = (delay: number) => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, delay)
+    })
+}
+
+axios.defaults.baseURL = "http://localhost:5000/api";
+
+const responseBody = <T>(response: AxiosResponse<T>) => response.data;
+
+axios.interceptors.response.use(async response => {
+    try {
+        await sleep(1000);
+        return response;
+    } catch (error) {
+        console.log(error);
+        return await Promise.reject(error);
+    }
+})
+
+const request = {
+    get: <T>(url: string) => axios.get<T>(url).then(responseBody),
+    post: <T>(url: string, body: {}) => axios.post<T>(url, body).then(responseBody),
+    put: <T>(url: string, body: {}) => axios.put<T>(url, body).then(responseBody),
+    delete: <T>(url: string) => axios.delete<T>(url).then(responseBody),
+}
+
+const Activities = {
+    list: () => request.get<Activity[]>('/activities'),
+    details : (id:string) => request.get<Activity>(`/activities/${id}`),
+    create : (activity: Activity) => axios.post<void>('/activities', activity),
+    update : (activity: Activity) => axios.put<void>(`/activities/${activity.id}`, activity),
+    delete : (id:string) => axios.delete<void>(`/activities/${id}`)
+}
+
+const agent = {
+    Activities
+}
+
+export default agent;
--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Grid} from "semantic-ui-react";
+import { Grid } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
 import ActivityDetails from "../details/ActivityDetails";
 import ActivityForm from "../form/ActivityForm";
@@ -14,15 +14,19 @@ interface Props {
     openForm: (id: string) => void;
     closeForm: () => void;
     createOrEdit: (activity: Activity) => void;
-    deleteActivity: (id:string) => void;
+    deleteActivity: (id: string) => void;
+    submitting: boolean;
 }
 
 export default function ActivityDashboard({ activities, selectedActivity, selectActivity,
-    cancelSelectActivity, editMode, openForm, closeForm, createOrEdit, deleteActivity }: Props) {
+    cancelSelectActivity, editMode, openForm, closeForm, createOrEdit, deleteActivity, submitting }: Props) {
     return (
         <Grid>
             <Grid.Column width='10'>
-                <ActivityList activities={activities} selectActivity={selectActivity} deleteActivity={deleteActivity} />
+                <ActivityList activities={activities}
+                    selectActivity={selectActivity}
+                    deleteActivity={deleteActivity}
+                    submitting={submitting} />
             </Grid.Column>
             <Grid.Column width="6">
                 {selectedActivity && !editMode &&
@@ -30,7 +34,11 @@ export default function ActivityDashboard({ activities, selectedActivity, select
                         cancelSelectActivity={cancelSelectActivity}
                         openForm={openForm} />}
                 {editMode &&
-                    <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit} />}
+                    <ActivityForm closeForm={closeForm}
+                        activity={selectedActivity}
+                        createOrEdit={createOrEdit}
+                        submitting={submitting}
+                    />}
             </Grid.Column>
         </Grid>
     )
