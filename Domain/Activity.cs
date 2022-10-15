9016fd66b3260f63e892393022d5bde6679da596
Ilija Sušac <isusac7695@gmail.com>
2022-10-15 09:46:54 +0200

    minor

--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -10,7 +10,7 @@ namespace Domain
         public DateTime Date {get; set; }   
         public string Description { get; set; }
         public string Category { get; set; }
-        public string City { get; set; }    
+        public string City { get; set; }
         public string Venue { get; set; }
         public bool IsCancelled { get; set; }
         public ICollection<ActivityAttendee> Attendees { get; set; } = new List<ActivityAttendee>();
