a6868d371ec3218a34e4d83d9def7db3dee3c5b4
Ilija Sušac <isusac7695@gmail.com>
2022-10-15 09:53:49 +0200

    c

--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -5,7 +5,7 @@ namespace Domain
 {
     public class Activity
     {
-        public Guid Id { get; set; }
+        public Guid Id { get; set; }     
         public string Title { get; set; }
         public DateTime Date {get; set; }   
         public string Description { get; set; }
